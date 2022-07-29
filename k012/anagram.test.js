6e3690b20ba1c8cca0e111d64e0c9e79432307eb
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-07-29 10:43:48 +0100

    add tests

---
+++ k012/anagram.test.js
@@ -0,0 +1,10 @@
+describe("Anagrams", () => {
+    it("should return a list of all anagrams", function () {
+        assert.deepEqual(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa']);
+        assert.deepEqual(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer']);
+    });
+
+    it("should return an empty array for no anagrams", function () {
+        assert.deepEqual(anagrams('laser', ['lazing', 'lazy', 'lacer']), []);
+    });
+});
