6abcc641632138e545cb83472ec549465dcfa7f2
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-07-29 15:49:19 +0100

    Finish k011 and rename k folders

---
+++ k011/anagram.js
@@ -0,0 +1,14 @@
+const anagrams = (word, listOfWords) => {
+  const splitedMainWord = word.split('').sort().join();
+  const joinedWords = [];
+  listOfWords.map((item, index) => {
+    if (item.split('').sort().join() == splitedMainWord) {
+      joinedWords.push(listOfWords[index])
+    }
+  });
+  return joinedWords;
+};
+
+module.exports = {
+  anagrams
+}
