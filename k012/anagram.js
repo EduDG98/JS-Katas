39f997786949dec1137fe6c91ec345fdbc10fdff
Jose <jose.diazg@protonmail.com>
2022-07-29 11:07:27 +0100

    test: add them for anagrams

--- k012/anagram.js
+++ k012/anagram.js
@@ -1,1 +1,12 @@
-console.log("hola")
+const anagrams = (word, listOfWords) => {
+  console.log('🥭');
+  // modificandolo
+  return 5;
+};
+
+const result = anagrams('qwe', [])
+console.log(result);
+
+module.exports = {
+  anagrams
+}
--- k012/anagram.test.js
+++ k012/anagram.test.js
@@ -1,10 +1,47 @@
+const anagramsModule = require('./anagram');
+
+const { anagrams } = anagramsModule;
+
 describe("Anagrams", () => {
-    it("should return a list of all anagrams", function () {
-        assert.deepEqual(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa']);
-        assert.deepEqual(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer']);
+    fit("should return a list of all anagrams - I", () => {
+            // AAA
+            // Arrange - configuras el test
+            const word = 'abba';
+            const listOfWords = ['aabb', 'abcd', 'bbaa', 'dada'];
+            const expected = ['aabb', 'bbaa'];
+            
+            // Act - hace las acciones necesarias
+            const sut = anagrams(word, listOfWords); // Subject under test
+            
+            // Assert - comprueba que el resultado de las acciones es lo debido
+            expect(sut).toEqual(expected);
+    });
+    it("should return a list of all anagrams - II", () => {
+        // AAA
+        // Arrange - configuras el test
+        const word = 'racer';
+        const listOfWords = ['crazer', 'carer', 'racar', 'caers', 'racer'];
+        const expected = ['carer', 'racer'];
+        
+        // Act - hace las acciones necesarias
+        const sut = anagrams(word, listOfWords); // Subject under test
+        
+        // Assert - comprueba que el resultado de las acciones es lo debido
+        expect(sut).toEqual(expected);
     });
 
-    it("should return an empty array for no anagrams", function () {
-        assert.deepEqual(anagrams('laser', ['lazing', 'lazy', 'lacer']), []);
-    });
+    it("should return an empty array for no anagrams", () => {
+            // AAA
+            // Arrange - configuras el test
+            const word = 'laser';
+            const listOfWords = ['lazing', 'lazy', 'lacer'];
+            const expected = [];
+
+            // Act - hace las acciones necesarias
+            const sut = anagrams(word, listOfWords); // Subject under test
+
+
+            // Assert - comprueba que el resultado de las acciones es lo debido
+            expect(sut).toEqual(expected);
+        });
 });
